6ea7a332f11ec7053f7adaa116e8caabef6dafe8
Tomas Kacalek <kacalek.tom@gmail.com>
2020-05-02 00:32:59 +0200

    Updates

---
+++ precache-manifest.6fbf7ce9348145ddc977f3e43092bc79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83ab61859c7e921e50e9b590708895e",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "171e82b3d789eef8c4fa",
+    "url": "/my-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44937aaf3a4fce9aa529",
+    "url": "/my-react-app/static/js/2.5d561851.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app/static/js/2.5d561851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171e82b3d789eef8c4fa",
+    "url": "/my-react-app/static/js/main.2f4c7760.chunk.js"
+  },
+  {
+    "revision": "ece3b83bce8d17fb8b2c",
+    "url": "/my-react-app/static/js/runtime-main.ddd33c01.js"
+  }
+]);
